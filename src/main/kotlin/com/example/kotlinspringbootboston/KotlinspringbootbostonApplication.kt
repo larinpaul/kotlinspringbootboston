4eac3a159acd786e403c79d09340422caccdded0
larinpaul <www.larinpavel@mail.ru>
2024-04-13 13:09:53 +0500

    Initial commit

---
+++ src/main/kotlin/com/example/kotlinspringbootboston/KotlinspringbootbostonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringbootboston
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinspringbootbostonApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinspringbootbostonApplication>(*args)
+}
