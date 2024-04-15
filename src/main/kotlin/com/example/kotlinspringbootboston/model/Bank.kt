ad88c449fda58de0b0d8f53b74d3594c96739101
larinpaul <www.larinpavel@mail.ru>
2024-04-15 08:03:07 +0500

    Kotlin uses "primary constructors", which allows you to define the parameters up there near the class declaration. Also all the val are already private, but the getters and setters for them are already public! the fields are "public" by default, but not like in Java. Also if you make your class data, you generate the extensive implementations of equals() hashCode() and toString(). Now we have what we initially wanted to have, a data transfer object... In Java 14+, we also have a similar feature called "records"

--- src/main/kotlin/com/example/kotlinspringbootboston/model/Bank.kt
+++ src/main/kotlin/com/example/kotlinspringbootboston/model/Bank.kt
@@ -1,43 +1,7 @@
 package com.example.kotlinspringbootboston.model
 
-class Bank {
-
-    private val accountNumber: String
-        get() = field // This is not necessary, since if you have a val, Kotlin will generate a default getter for you
-//        set(value) { field = value }
-    private val trust: Double
-    private val transactionFee: Int
-
-    constructor(accountNumber: String, trust: Double, transactionFee: Int) {
-        this.accountNumber = accountNumber
-        this.trust = trust
-        this.transactionFee = transactionFee
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Bank
-
-        if (accountNumber != other.accountNumber) return false
-        if (trust != other.trust) return false
-        if (transactionFee != other.transactionFee) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = accountNumber.hashCode()
-        result = 31 * result + trust.hashCode()
-        result = 31 * result + transactionFee
-        return result
-    }
-
-    override fun toString(): String {
-        return "Bank(accountNumber='$accountNumber', trust=$trust, transactionFee=$transactionFee)"
-    }
-
-
-
-}
+data class Bank(
+    val accountNumber: String,
+    val trust: Double,
+    val transactionFee: Int
+)
