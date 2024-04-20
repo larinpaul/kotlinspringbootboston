5f1dee63ea8e52405691b29dac254678f189fa69
larinpaul <www.larinpavel@mail.ru>
2024-04-20 10:21:40 +0500

    Started creating a controller and writing the MVC test

--- src/main/kotlin/com/example/kotlinspringbootboston/controller/BankController.kt
+++ src/main/kotlin/com/example/kotlinspringbootboston/controller/BankController.kt
@@ -1,6 +1,15 @@
 package com.example.kotlinspringbootboston.controller
 
+import com.example.kotlinspringbootboston.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BankController
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): String = "works"
+
+}
